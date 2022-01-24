6be4ab69b1418eb5530738cbcbe7981b487737f8
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-24 20:11:28 +0300

    Styled the footer

--- whip/src/components/FooterComponent.js
+++ whip/src/components/FooterComponent.js
@@ -23,14 +23,14 @@ function Footer(){
                         <div class = "card"><img src="..." class="card-img-top" alt="..."></img></div>
                     </div>
                 </div>
-                <div class = "row">
+            </div>
+            <div class = "row">
                     <div class = "col-md-12 text-center">
                         <p>Are you a - <span>Manager</span>- <span>Music label</span> - <span>Agent</span> - <span>Promoter</span> or an <span>Artist</span>?</p>
-                        <p><span>DISCOVER</span> <span>ACCESS</span> <span>EVALUATE DATA</span></p>
+                        <p><span>DISCOVER. </span> <span>ACCESS. </span> <span>EVALUATE DATA. </span></p>
                         <button class="btn mt-4 btn-light btn-lg rounded-pill" type="submit" >SIGN UP</button>
                         <p> &copy; COPYRIGHT 2022 WHIP AFRICA MUSIC</p>
                     </div>
-                </div>
             </div>
         </div>
       ) 
