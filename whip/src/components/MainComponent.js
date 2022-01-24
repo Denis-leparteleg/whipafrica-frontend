5aecbd0ef6eae6299a98b7d87425dd83c99e80e3
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-24 17:42:09 +0300

    components added

---
+++ whip/src/components/MainComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './HeaderComponent'
+import Footer from './FooterComponent'
+
+
+
+function Main (){
+     return(
+         <React.Fragment>
+         <Header/>
+         <div>Hello World</div>
+         <Footer/>
+         </React.Fragment>
+     )
+}
+
+export default Main;
