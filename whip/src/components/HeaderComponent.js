825f7067119f4f780e3137b9925c23905c6cdb86
clara chepngeno metto <clara.metto@student.moringaschool.com>
2022-01-25 19:32:41 +0300

    create navbar, added links, logo and styled it

--- whip/src/components/HeaderComponent.js
+++ whip/src/components/HeaderComponent.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import '../index.css';
+import whip from '../assets/logo2.jpeg';
 
 
 function Header(){
       return(
           <div>
-              {/* Header Component */}
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    <img class="logo" src={whip} width="45" height="40" alt=""/>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav ml-auto" >
+                    <li class="nav-item active">
+                        <a class="nav-link " id="home" href="#">HOME<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" id="art" href="#">ARTIST DASHBOARD</a>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
           </div>
       )
 }
