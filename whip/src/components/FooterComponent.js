33044da1566257151886f5abf4cec9d0eb01bb48
Victoria045 <victoriaberyl45@gmail.com>
2022-01-25 11:56:41 +0300

    nest the footer component

--- whip/src/components/FooterComponent.js
+++ whip/src/components/FooterComponent.js
@@ -3,7 +3,9 @@ import React from 'react';
 
 function Footer(){
       return(
-          <div>Footer Component</div>
+          <div>
+              {/* Footer Component */}
+              </div>
       ) 
 }
 export default Footer;
