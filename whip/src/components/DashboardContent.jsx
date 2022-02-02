fb30b0b452a6b26a618219e74bb0eea8f4c86407
Victoria045 <victoriaberyl45@gmail.com>
2022-02-02 19:49:25 +0300

    modify dashboard design

--- whip/src/components/DashboardContent.jsx
+++ whip/src/components/DashboardContent.jsx
@@ -1,183 +1,223 @@
 import React, {useState, useEffect } from "react";
 import axios from "axios";
-import DashboardChart from "./DashboardChartComponent";
+
 const DashboardContent = ()=>{
     const [artists, setArtist] = useState([]);
     const [tracks, setTracks] = useState([]);
     const [searchTerm, setSearchTerm] = useState();
-    // useEffect(() => {
-    //     handleSubmit();
-    //   }, []);
-    // var axios = require("axios").default;
-    // async function handleSubmit(e) {
+
+    useEffect(() => {
+        // handleSubmit();
+      }, []);
+
+    // async function handleSubmit() {
     // e.preventDefault();
-    // var options = {
-    //     method: 'GET',
-    //     url: 'https://shazam.p.rapidapi.com/search',
-    //     params: {term: searchTerm, locale: 'en-US', offset: '0', limit: '5'},
-    //     headers: {
-    //         'x-rapidapi-host': 'shazam.p.rapidapi.com',
-    //         'x-rapidapi-key': '5627f8a25bmshea5cb0a9f785954p10c77djsndd9e06d038b6'
-    //     }
-    //     };
-    // axios.request(options).then(function (response) {
-    //     // console.log(response);
-    //     console.log(response.data);
-    //     // console.log(response.data.artists.hits);
-    //     console.log(response.data.tracks.hits);
-    //     setArtist(response?.data?.artists?.hits);
-    //     setTracks(response?.data?.tracks?.hits);
-    // }).catch( (error) =>{
-    //     console.log(error)
-    //     console.error(error.response.data);
-    // });
-    // axios(options);
-    // handleSubmit();
-    // }
-    // var options2 = {
-    //     method: 'GET',
-    //     url: 'https://shazam.p.rapidapi.com/charts/track',
-    //     params: {locale: 'en-US', pageSize: '20', startFrom: '0'},
-    //     headers: {
-    //       'x-rapidapi-host': 'shazam.p.rapidapi.com',
-    //       'x-rapidapi-key': '0d5e1bc88cmsh44b756ceca88c70p1580d6jsna4720860568f'
-    //     }
-    //   };
-    //   axios.request(options2).then(function (response) {
-    //     console.log(response);
-    //       console.log(response.data);
-    //   }).catch(function (error) {
-    //       console.error(error);
-    //   });
+
     // const axios = require("axios");
+
     const fetchData = async () => {
     let res = await axios({
-    method: "GET",
-    url: "https://songstats.p.rapidapi.com/artists/stats?source=all&spotify_artist_id=2h93pZq0e7k5yf4dywlkpM",
-    headers: {
-      "x-rapidapi-host": "songstats.p.rapidapi.com",
-      "x-rapidapi-key": "53f05c0b1amsh90257705c3be42cp14d383jsn20b8197584af",
-    },
-  });
-  console.log(res);
-  console.log(res.status);
-  console.log(res.data);
-  let newd = res.data.stats;
-  newd.forEach((doc) => {
-    console.log(doc);
-  });
-};
-fetchData();
+        method: "GET",
+        // url: "https://songstats.p.rapidapi.com/artists/stats?source=all&spotify_artist_id=2h93pZq0e7k5yf4dywlkpM",
+        headers: {
+        // "x-rapidapi-host": "songstats.p.rapidapi.com",
+        // "x-rapidapi-key": "53f05c0b1amsh90257705c3be42cp14d383jsn20b8197584af",
+        },
+    });
+    console.log(res);
+    console.log(res.status);
+    console.log(res.data);
+    let newd = res.data.stats;
+    newd.forEach((doc) => {
+        console.log(doc);
+    });
+    };
+    fetchData();
+    //   };   
+    // handleSubmit();   
+    // };
+    
     return(
         <div>
             <div class = "row">
-                <div class = "col-md-5 top-right">
-                    <p>Fresh Talent <span>Dashboard</span></p>
+                <div class = "col-md-4 top-right">
+                    <h3>Fresh Talent <span>Dashboard</span></h3>
                     <p>whipafrica.com/freshtalent</p>
                 </div>
-                <div class = "col-md-2"></div>
-                <div class = "col-md-5 top-left">
-                    <div>
+                {/* <div class = "col-md-2"></div> */}
+                <div class = "col-md-8 top-left">
+                    {/* <div>
                         <button type="submit" className="btn btn-primary btn-block btn-lg rounded-pill">Fresh Talent List</button>
-                    </div>
-                    {/* <div class="search-container">
-                        <form method="get">
-                            <label for="song"></label>
-                            <input type="text" name="keyword" id="keyword" placeholder="   Search for a song or artist"/>
-                            <button class="btn btn-primary search-btn" type="submit"><span>Search for spotify id </span></button>
-                        </form>
                     </div> */}
-                    <div class="search-container">
-                        <form method="get">
+
+                    <div className="search-container">
+                        <form method="get" className="d-flex justify-content-between p-2">
                             <label for="song"></label>
-                            <input type="text" value={searchTerm}  onChange={({ target }) => setSearchTerm(target.value)} className="form-control" placeholder="Search for a song or artist" />
-                            {/* <button class="btn btn-primary search-btn" type="submit" onClick={handleSubmit}><span>Search for artist</span></button> */}
+                            <input type="text" value={searchTerm}  onChange={({ target }) => setSearchTerm(target.value)} className="form-control" placeholder="Search for an artist" />
+                            <button className="btn btn-primary search-btn mx-1" type="submit"><span>SEARCH</span></button>
                         </form>
                     </div>
+
                     <div class = "top-left-left">
                          <div>
-                             {/* <img src="../images/bensoul.jpeg" class="card-img-top" alt="..." height = "40px" width="40px"></img> */}
-                             {artists.map((artist) => (
+                             <img src="../images/bensoul.jpeg" class="card-img-top" alt="..." height = "40px" width="40px"></img>
+                             {/* {artists.map((artist) => (
                                 <div key={artist.id} className='card cd-img'>
                                     <img src={artist.artist.avatar} alt="..." height = "60px" width="60px"/>
                                 </div>
-                                ))}
-                             </div>
-                         <div>
+
+                                ))} */}
+                         </div>
+                         
+                    <div>
                          <p>{artists.map((artist) => (
                                 <div key={artist.id}>
                                     <p>{artist.artist.name}</p>
                                 </div>
+
                                 ))}
-                             <i class="fa fa-fw fa-chevron-down"></i></p>
+                             {/* <i class="fa fa-fw fa-chevron-down"></i> */}
+                             </p>
                          {/* <p>A$R Scout</p> */}
                          </div>
                     </div>
                 </div>
             </div>
-            <div class="row">
-        <div class="col-md-2 offset-md-1">
+            <div className="row col-row text-center">
+        <div class="col-md-1">
         <div class="card">
-            {/* <img src="../images/bensoul.jpeg" class="card-img-top" alt="..." height = "250px"></img> */}
-            {artists.map((artist) => (
+            <img src="../images/bensoul.jpeg" class="card-img-top" alt="..." height = "200px" width = "40px"></img>
+            {/* {artists.map((artist) => (
             <div key={artist.id} className='card cd-img'>
                 <img src={artist.artist.avatar} className="card-img-top" alt="..." height = "250px" />
             </div>
-            ))}
+            ))} */}
+
         </div>
         </div>
-        <div class="col-md-2">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
+        <div className="col-md-2">
+            <div className="">
+                <div className="card-body">
+                    <h3 className="card-title text-center">AFIA</h3>
+                    <p className="text-center">Genre: R&B<br/>
+                    <span>Location:Abuja</span></p>
+                    <div className="text-center">
+                    <button className="rounded-pill btn-sm">Contact Artist</button>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-2">
-            <div class="card">
+        <div className="col-md-3">
+            <div class="card btn-r">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
+                    <div className="text-center">
+                    <button className="p-3 btn-r"><i class="fa fa-clock-o fa-2x" aria-hidden="true"></i></button>
+                    </div>
+                    <p class="card-title card-center">Average Streams Per Month<br/>
+                    <span className="text-span">3.4M</span></p>
                 </div>
             </div>
         </div>
-        <div class="col-md-2">
-            <div class="card">
+        <div class="col-md-3 ">
+            <div class="card btn-r">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
+                <div className="text-center">
+                <button  className="p-3 btn-r"><i class="fa fa-clock-o fa-2x" aria-hidden="true"></i></button>
+                </div>
+                <p class="card-title card-center">Streams Growth Rate<br/>
+                    <span className="text-span">31.58</span></p>
                 </div>
             </div>
         </div>
-        <div class="col-md-2 ">
-            <div class="card">
+        <div class="col-md-3 ">
+            <div class="card btn-r">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
+                <div className="text-center">
+                <button className="p-3 btn-r"><i class="fa fa-clock-o fa-2x" aria-hidden="true"></i></button>
+                </div>
+                <p class="card-title card-center">Social Media Following<br/>
+                    <span className="text-span">12.9</span></p>
                 </div>
             </div>
         </div>
     </div>
-                <ul>
+    <div className="grid-container">
+        <div className="grid-item item1 p-5">
+            <h5>Streams over the last 1 year</h5>
+            <img src="../images/chart2.png" class="card-img-top" alt="..."></img>
+        </div>
+        <div className="grid-item item2">
+        <img src="../images/chart1.jpeg" class="card-img-top" alt="..."></img>
+        </div>
+        <div className="grid-item item3">
+        <div class="w3-container">
+  
+  <table class="w3-table w3-bordered w3-centered">
+    <tr>
+      <th>Other Key Stats</th>
+      <th>Current</th>
+      <th>Last Month</th>
+      <th>Average Growth Rate (6 months)</th>
+    </tr>
+    <tr>
+      <td>Most popular song</td>
+      <td>6</td>
+      <td>135k</td>
+      <td>1.87%</td>
+    </tr>
+    <tr>
+      <td>Most popular YouTube video</td>
+      <td>1</td>
+      <td>33k</td>
+      <td>7.35%</td>
+    </tr>
+    <tr>
+      <td>Followers on leading social network</td>
+      <td>1</td>
+      <td>74k</td>
+      <td>16.44%</td>
+    </tr>
+    <tr>
+      <td>Engagement Rate</td>
+      <td>1</td>
+      <td>74k</td>
+      <td>16.44%</td>
+    </tr>
+    <br></br>
+  </table>
+</div>
+        </div>
+        <div className="grid-item item4 p-3">
+            <h4 className="prob">Probability of Profitability ...</h4>
+        </div>
+    </div>
+                {/* <ul>
                 {tracks.map((track) => (
-                    <li key={track.id} className='card cd-img'>
-                        <h1>{track.track.title} </h1>
-                        <h1>{track.track.url} </h1>
+                    <li key={track.id} className='card card-wr cd-img mb-2'>
+                        <img src={track.track.images.background} className="card-img-top" alt="..." height = "250px" />
+                        <h4>{track.track.title} <a><span className="text-primary">{track.track.url}</span></a></h4>
                     </li>
-                    ))}
-                </ul>
+                    ))} 
+                </ul> */}
+
                 {/* <ul>
                 {tracks.map((track) => (
                     <li key={track.id} className='card cd-img'>
                         <h1>{track.track.title} </h1>
                     </li>
-                    ))}
+                    ))} 
                 </ul> */}
+                
                 {/* {tracks.map((track) => (
                     <div key={track.id} className='card cd-img'>
                         <h1>{track.track.title} </h1>
                     </div>
+
                     ))} */}
 
-        {/* <DashboardChart/>             */}
         </div>
     )
+
 }
+
 export default DashboardContent;
