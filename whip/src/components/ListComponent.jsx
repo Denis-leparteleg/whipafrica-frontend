007b4de4b37f9aabe6aad7ba43f2a2d0346f3f16
Annaliskirwa <annaliskirwa@gmail.com>
2022-02-08 13:47:50 +0300

    Added artists in the list

--- whip/src/components/ListComponent.jsx
+++ whip/src/components/ListComponent.jsx
@@ -30,6 +30,58 @@ const ListComponent = ()=>{
                     <a href="#">Artist Deep Dive</a>
                 </div>
                 <hr />
+                <div className="list-cont text-center">
+                    <h3 className="pt-5"><b>3</b></h3>
+                    <img src="../images/phy.jpeg" alt="..." height = "120px" width="140px"/>
+                    <h3><b>Phy</b></h3>
+                    <h3><b>13K</b><br/><br/>
+                    <span className="span-list">Average Streams per month</span></h3>
+                    <h3 className=""><b>34%</b><br/><br/>
+                    <span className="span-list">Streams Growth Rate</span></h3>
+                    <h3><b>9.1K</b><br/><br/>
+                    <span className="span-list">Social Media Following</span></h3>
+                    <a href="#">Artist Deep Dive</a>
+                </div>
+                <hr />
+                <div className="list-cont text-center">
+                    <h3 className="pt-5"><b>4</b></h3>
+                    <img src="../images/stromae.jpeg" alt="..." height = "120px" width="140px"/>
+                    <h3><b>Stromae</b></h3>
+                    <h3><b>10K</b><br/><br/>
+                    <span className="span-list">Average Streams per month</span></h3>
+                    <h3 className=""><b>27%</b><br/><br/>
+                    <span className="span-list">Streams Growth Rate</span></h3>
+                    <h3><b>7.1K</b><br/><br/>
+                    <span className="span-list">Social Media Following</span></h3>
+                    <a href="#">Artist Deep Dive</a>
+                </div>
+                <hr />
+                <div className="list-cont text-center">
+                    <h3 className="pt-5"><b>5</b></h3>
+                    <img src="../images/bien.jpg" alt="..." height = "120px" width="140px"/>
+                    <h3><b>Bien</b></h3>
+                    <h3><b>7K</b><br/><br/>
+                    <span className="span-list">Average Streams per month</span></h3>
+                    <h3 className=""><b>19%</b><br/><br/>
+                    <span className="span-list">Streams Growth Rate</span></h3>
+                    <h3><b>5.2K</b><br/><br/>
+                    <span className="span-list">Social Media Following</span></h3>
+                    <a href="#">Artist Deep Dive</a>
+                </div>
+                <hr />
+                <div className="list-cont text-center">
+                    <h3 className="pt-5"><b>6</b></h3>
+                    <img src="../images/karun.jpg" alt="..." height = "120px" width="140px"/>
+                    <h3><b>Karun</b></h3>
+                    <h3><b>3K</b><br/><br/>
+                    <span className="span-list">Average Streams per month</span></h3>
+                    <h3 className=""><b>13%</b><br/><br/>
+                    <span className="span-list">Streams Growth Rate</span></h3>
+                    <h3><b>3.0K</b><br/><br/>
+                    <span className="span-list">Social Media Following</span></h3>
+                    <a href="#">Artist Deep Dive</a>
+                </div>
+                <hr />
         </div>
     )
 };
